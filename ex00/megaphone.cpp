c3c04955ba4d5c1521ac332d1c0a5e857a3fd7d8
Sylsee <sylvio.poliart@gmail.com>
2022-03-04 13:21:53 +0100

    clean

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: spoliart <spoliart@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 15:00:07 by spoliart          #+#    #+#             */
-/*   Updated: 2022/01/24 17:02:37 by spoliart         ###   ########.fr       */
+/*   Updated: 2022/03/04 13:21:06 by spoliart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,15 @@
 
 int main(int argc, char **argv)
 {
-    if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    else {
-        for (int i = 1; i < argc; i++) {
-            for (int j = 0; argv[i][j]; j++) {
-                if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
-                    std::cout << toupper(argv[i][j]);
-                else
-                    std::cout << argv[i][j];
-            }
-        }
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else {
+		for (int i = 1; i < argc; i++) {
+			for (int j = 0; argv[i][j]; j++) {
+				std::cout << (unsigned char)toupper(argv[i][j]);
+			}
+		}
 		std::cout << std::endl;
-    }
-    return (0);
+	}
+	return (0);
 }
